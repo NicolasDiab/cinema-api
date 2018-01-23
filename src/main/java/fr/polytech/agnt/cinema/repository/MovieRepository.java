42b51f4a8b173563743594b1a2bee38453af9986
agory <gory.alexandre02@gmail.com>
2018-01-23 19:58:13 +0100

    update movie repository(search query)

--- src/main/java/fr/polytech/agnt/cinema/repository/MovieRepository.java
+++ src/main/java/fr/polytech/agnt/cinema/repository/MovieRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface MovieRepository extends CrudRepository<Movie, Integer> {
-    @Query("SELECT m FROM Movie m inner join m.category c where title like %:text% or c.name like %:text%")
+    @Query("SELECT m FROM Movie m join m.category c join m.director d where title like %:text% or c.name like %:text% or d.firstname like %:text% or d.lastname like %:text%")
     public List<Movie> search(@Param("text") String text);
 }
